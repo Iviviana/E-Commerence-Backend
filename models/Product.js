bfeb52bd76e3ddce99d6e7bdb5664a530f27d8ce
Antonia Garci <antonia@Antonias-MacBook-Air.local>
2022-02-25 22:18:03 -0500

    added routes and seeds and functions as should

--- models/Product.js
+++ models/Product.js
@@ -9,47 +9,50 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-
-    id:{
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    product_name:{
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    price: {
-      type: DataTypes.DECIMAL(10,2),
-      allowNull: false,
-      validate: {
-        isDecimal: true
+      // define an id column
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      // define product_name column
+      product_name: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      // define price column
+      price: {
+          type: DataTypes.DECIMAL(10,2),
+          allowNull: false,
+          validate: {
+              isDecimal: true
+          }
+      },
+      // define stock column
+      stock: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 10,
+          validate: {
+              isNumeric: true
+          }
+      },
+      // define category_id column
+      category_id: {
+          type: DataTypes.INTEGER,
+          references: {
+              model: "category",
+              key: "id"
+          }
       }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "catergory",
-        key: "id"
-      }
-    }
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'product',
   }
 );
 
